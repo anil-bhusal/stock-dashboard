786f0535726001446054bd80b2d4687bbd9d570f
anil-bhusal <anilbhusal0904@gmail.com>
2025-04-26 22:23:39 +1200

    added seed file

---
+++ stock-dashboard-backend/src/seed/seed.ts
@@ -0,0 +1,32 @@
+import { DataSource } from 'typeorm';
+import { stocks } from './stock.seed';
+import { Stock } from 'src/stock/entities/stock/stock';
+
+const AppDataSource = new DataSource({
+    type: 'postgres', // or mysql/mariadb/sqlite
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USERNAME,
+    password: `${process.env.DB_PASSWORD}`,
+    database: process.env.DB_NAME,
+    entities: [Stock],
+    synchronize: true, // ONLY FOR DEV, NOT IN PRODUCTION
+});
+
+async function seed() {
+    await AppDataSource.initialize();
+
+    for (const stock of stocks) {
+        const repo = AppDataSource.getRepository(Stock);
+        const newStock = repo.create(stock);
+        await repo.save(newStock);
+    }
+
+    console.log('Seeding finished');
+    await AppDataSource.destroy();
+}
+
+seed().catch((err) => {
+    console.error('Seeding error', err);
+    process.exit(1);
+});
